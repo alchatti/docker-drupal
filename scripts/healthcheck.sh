105dba5e60d87be7113cb253e6db5b6937fe7d58
Majed Al-Chatti <majed@alchatti.com>
2026-06-18 14:29:19 +0400

    Update healthcheck.sh

--- scripts/healthcheck.sh
+++ scripts/healthcheck.sh
@@ -8,7 +8,7 @@ cd "$DOC_ROOT"
 curl -fsS --max-time 5 http://127.0.0.1/ >/dev/null
 
 # 2. Check Drupal bootstrap and database
-drush status bootstrap --field=bootstrap | grep -q "Successful"
+drush status --field=bootstrap | grep -q "Successful"
 
 # 3. Run simple DB query through Drupal
 drush sql:query "SELECT 1;" >/dev/null
