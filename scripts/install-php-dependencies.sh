6fb3daf7ac86b2146e2de962e0b5ea490fe3de62
Majed Al-Chatti <majed@alchatti.com>
2026-06-19 17:11:02 +0400

    Update install-php-dependencies.sh
    Added unzip

--- scripts/install-php-dependencies.sh
+++ scripts/install-php-dependencies.sh
@@ -15,6 +15,7 @@ savedAptMark="$(apt-mark showmanual)"
 
 apt-get update
 apt-get install -y --no-install-recommends \
+    unzip \
     libfreetype6-dev \
     libicu-dev \
     libjpeg62-turbo-dev \
